28fd6755c8c9708043d52700181c332070e13ed2
HuaijiaLin <huaijialin@gmail.com>
2021-11-20 15:35:05 +0800

    update train

---
+++ scripts/YTV2019/run_vis_x101.sh
@@ -0,0 +1,3 @@
+python tools/train.py \
+    configs/SeqMaskRCNN/seq_mask_rcnn_x101_fpn_1x_youtubevos19_ftstage.py \
+    --gpu='0,1,2,3'
---
+++ scripts/YTV2021/run_vis_x101.sh
@@ -0,0 +1,3 @@
+python tools/train.py \
+    configs/SeqMaskRCNN/seq_mask_rcnn_x101_fpn_1x_youtubevos21_ftstage.py \
+    --gpu='0,1,2,3'
